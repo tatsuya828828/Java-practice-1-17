72eebab65a85b633b94c663c7e4f2c936a19c097
tatsuya828828 <kkjy011894@gmail.com>
2020-07-01 12:09:40 +0900

    [finish]17-4

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,14 @@ import java.io.IOException;
 public class Main {
 	public static void main(String[] args) {
 		try {
-			FileWriter fw = new FileWriter("data.txt");
+			FileWriter fw = new FileWriter("data.t");
 			fw.write("hello!");
 			fw.close();
 		} catch(IOException e) {
-			System.out.println("エラーです。中断します。");
+			// getMessage()で例外的状況のエラー文を取得
+			System.out.println("エラー"+e.getMessage());
+			// printStackTrace()でスタックトレースの内容を出力
+			e.printStackTrace();
 		}
 	}
 }
