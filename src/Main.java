b8ebd8332497a4981796852a8c110cb614c4fae3
tatsuya828828 <kkjy011894@gmail.com>
2020-07-01 13:26:45 +0900

    [finish]17-7

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,6 @@
-import java.io.FileWriter;
-import java.io.IOException;
-
 public class Main {
 	public static void main(String[] args) {
-		try(FileWriter fw = new FileWriter("data.txt");) {
-			fw.write("hello!");
-		} catch(IOException e) {
-			// getMessage()で例外的状況のエラー文を取得
-			System.out.println("エラー"+e.getMessage());
-			// printStackTrace()でスタックトレースの内容を出力
-			e.printStackTrace();
-		}
+		Person p = new Person();
+		p.setAge(128);
 	}
 }
